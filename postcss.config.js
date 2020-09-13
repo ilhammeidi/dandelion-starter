5f120c16657048fbffd39267743de402f0536b97
ilhammeidi <ilham.meidi@gmail.com>
2020-09-13 16:14:31 +0700

    fixxed browser list warning

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,6 @@ var autoprefixer = require('autoprefixer'); // eslint-disable-line
 
 module.exports = {
   plugins: [
-    autoprefixer({ browsers: 'last 2 versions' })
+    autoprefixer({ overrideBrowserslist: 'last 2 versions' })
   ]
 };
