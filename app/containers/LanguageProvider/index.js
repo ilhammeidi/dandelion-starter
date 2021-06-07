39e63b024735b6b82c61c677d80c36ba50ce4eda
ilhammeidi <ilham.meidi@gmail.com>
2021-06-07 19:56:17 +0700

    remove offline plugin by default

--- app/containers/LanguageProvider/index.js
+++ app/containers/LanguageProvider/index.js
@@ -6,6 +6,8 @@
  * IntlProvider component and i18n messages (loaded from `app/translations`)
  */
 
+/* eslint-disable */
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
@@ -14,39 +16,29 @@ import { IntlProvider } from 'react-intl';
 
 import { makeSelectLocale } from './selectors';
 
-export class LanguageProvider extends React.PureComponent {
-  // eslint-disable-line react/prefer-stateless-function
-  render() {
-    const { locale, messages, children } = this.props;
-    return (
-      <IntlProvider
-        locale={locale}
-        key={locale}
-        messages={messages[locale]}
-      >
-        {React.Children.only(children)}
-      </IntlProvider>
-    );
-  }
+export function LanguageProvider(props) {
+  return (
+    <IntlProvider
+      locale={props.locale}
+      key={props.locale}
+      messages={props.messages[props.locale]}
+    >
+      {React.Children.only(props.children)}
+    </IntlProvider>
+  );
 }
 
 LanguageProvider.propTypes = {
-  locale: PropTypes.string.isRequired,
-  messages: PropTypes.object.isRequired,
+  locale: PropTypes.string,
+  messages: PropTypes.object,
   children: PropTypes.element.isRequired,
 };
 
-const mapStateToProps = createSelector(makeSelectLocale(), locale => ({
-  locale,
-}));
+const mapStateToProps = createSelector(
+  makeSelectLocale(),
+  locale => ({
+    locale,
+  }),
+);
 
-function mapDispatchToProps(dispatch) {
-  return {
-    dispatch,
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(LanguageProvider);
+export default connect(mapStateToProps)(LanguageProvider);
